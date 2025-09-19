a3c84c659c4a708a74d13121c56101455810f13e
Ziad Fellahi <ziad.fellahi@outlook.fr>
2025-09-19 13:59:56 +0200

    v1

---
+++ consCptBq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libCptBq;
+
+namespace consCptBq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Au boulot");
+        }
+    }
+}
---
+++ libCptBq/Compte.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libCptBq
+{
+    /// <summary>
+    /// Classe Compte
+    /// </summary>
+    public class Compte
+    {
+        private int numero;
+        private string nom;
+        private decimal solde;
+        private decimal decouvertAutorise; // valeur négative
+
+        /// <summary>
+        /// Propriétés implémentées automatiquement
+        /// </summary>
+        public int Numero { get; set; }
+        public string Nom { get; set; }
+        public decimal Solde { get; set; }
+        public decimal DecouvertAutorise { get; set; }
+
+
+        /// <summary>
+        /// Constructeur à 4 arguments
+        /// </summary>
+        /// <param name="numero">le numéro</param>
+        /// <param name="nom">le nom</param>
+        /// <param name="solde">le solde</param>
+        /// <param name="decouvertAutorise">le découvert autorisé</param>
+        public Compte(int numero, string nom, decimal solde, decimal decouvertAutorise)
+        {
+
+        }
+        /// <summary>
+        /// Constructeur de compte par défaut
+        /// </summary>
+        public Compte()
+        {
+
+        }
+        /// <summary>
+        /// Réecriture de la méthode ToString
+        /// </summary>
+        /// <returns></returns>
+
+
+        /// <summary>
+        /// Crédite le compte du montant spécifié
+        /// </summary>
+        /// <param name="montant">Le montant à créditer</param>
+        public void Crediter(decimal montant)
+        {
+
+        }
+
+        /// <summary>
+        /// Débite le compte du montant spécifié si le solde le permet
+        /// </summary>
+        /// <param name="montant">Le montant à débiter</param>
+        /// <returns>True si le débit a été effectué, False sinon</returns>
+        public bool Debiter(decimal montant)
+        {
+            return false;
+        }
+
+
+        /// <summary>
+        /// Transférer un montant vers un autre compte
+        /// </summary>
+        /// <param name="montant"></param>
+        /// <param name="compteDestination"></param>
+        /// <returns></returns>
+        public bool Transferer(decimal n, Compte c)
+        {
+            return false; 
+        }
+
+
+        /// <summary>
+        /// Savoir si le solde est supérieur à celui d'un autre compte
+        /// </summary>
+        /// <param name="compteDestination"></param>
+        /// <returns></returns>
+        public bool Superieur(Compte c)
+        {
+            return false;
+        }
+
+    }
+}
